1d77c268420ccb017a7218d98084c60150aa3afe
Victoria Beasley <iesnoth91@gmail.com>
2022-12-20 21:35:54 -0600

    find all, find one, and post working

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -16,52 +16,48 @@ events.get('/', async (req, res) => {
 })
 
 
-// //find a specific band
-// bands.get('/:id', async (req, res) => {
-//     try {
-//         const foundBand = await Band.findOne({
-//             where: { band_id: req.params.id }
-//         })
-//         res.status(200).json(foundBand)
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
+//find a specific band
+events.get('/:id', async (req, res) => {
+    try {
+        const foundEvent = await Event.findOne({
+            where: { event_id: req.params.id }
+        })
+        res.status(200).json(foundEvent)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
-// //create
-// bands.post('/', async (req, res) => {
-//     try {
-//         //this method accepts an object as its argument that specifies
-//         //which columns and values to create. We can assume anyone using
-//         //this route will send back a request body with that information.
-//         //Therefore we can pass in the req.body as the argument.
-//         const newBand = await Band.create(req.body)
-//         res.status(200).json({
-//             message: 'Successfully inserted new band',
-//             data: newBand
-//         })
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
+//create
+events.post('/', async (req, res) => {
+    try {
+        const newEvent = await Event.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted new event',
+            data: newEvent
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
-// // UPDATE A BAND
-// //why isn't this working?
-// bands.put('/:id', async (req, res) => {
-//     try {
-//         const updatedBands = await Band.update(req.body, {
-//             where: {
-//                 band_id: req.params.id
-//             }
-//         }
-//         )
-//         res.status(200).json({
-//             message: `Successfully updated ${updatedBands} band(s)`
-//         })
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
+// UPDATE events
+//why isn't this working?
+events.put('/:id', async (req, res) => {
+    try {
+        const updatedEvents = await Event.update(req.body, {
+            where: {
+                event_id: req.params.id
+            }
+        }
+        )
+        res.status(200).json({
+            message: `Successfully updated ${updatedEvents} events(s)`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 
 // //delete bands
